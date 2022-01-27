6d62d144c9a66f7b2fbeaec7bf90bd609247324e
AWM <dev@awm.me.uk>
2022-01-27 08:44:08 +0000

    Authorisation working!

--- glowmarkt.py
+++ glowmarkt.py
@@ -1,63 +1,69 @@
 #!/usr/bin/env python3
 
-import requests,json,re
+import requests, re
+from requests.exceptions import ConnectionError, HTTPError, Timeout, TooManyRedirects
 import logging as log
 
-class Glowmarkt:
+class Glowmarkt(object):
     URL='https://api.glowmarkt.com/api/v0-1/' # The API URL
-    token = None
+    authObj = None
     headers = {
         'token': None,
         'applicationId': 'b0f1b774-a586-4f72-9edd-27ead8aa7a8d'
     }
-    jwt = re.compile('^([a-zA-Z0-9]+\.){2}([a-zA-Z0-9]+)$')
+    jwtCheck = re.compile('^([a-zA-Z0-9]+\.){2}([a-zA-Z0-9]+)$')
+    appIdCheck = re.compile('^[a-f0-9-]+$')
     
     def authorise(self, u=None, p=None, t=None, appId=None):
         endpoint = self.URL + 'auth'
         reauth = False
         data = None
+        response = None
         
-        if not (u or p or t) and self.headers.token:
-            log.info('{}.authorise(): reauthorising existing token'.format(self.__name__))
+        if not (u or p or t) and self.headers['token']:
+            log.info('Glowmarkt.authorise(): reauthorising existing token')
             reauth = True
         elif t:
-            assert jwt.match(t), 'token (t) must be a valid JSON Web Token (JWT)'
-            log.info('{}.authorise(): reauthorising provided token'.format(self.__name__))
-            self.headers.token = t
+            assert self.jwtCheck.match(t), 'token (t) must be a valid JSON Web Token (JWT)'
+            log.info('Glowmarkt.authorise(): reauthorising provided token')
+            self.headers['token'] = t
             reauth = True
         else:
-            assert u.len() > 0 and p.len() > 0, 'Username (u) and password (p) must be non-empty strings'
-            log.info('{}.authorise(): authorising user {}'.format(self.__name__,u))
+            assert len(u) > 0 and len(p) > 0, 'Username (u) and password (p) must be non-empty strings'
+            log.info('Glowmarkt.authorise(): authorising user {}'.format(u))
             data = {'username': u, 'password': p}
         
         if appId:
-            assert appId.len() > 0, 'appId must be a non-empty string'
-            self.headers.applicationId = appId
+            assert self.appIdCheck.match(appId), 'appId must be a hexadecimal string'
+            self.headers['applicationId'] = appId
         
         try:
             if reauth:
                 response = requests.get(endpoint,headers=self.headers)
             else:
                 response = requests.post(endpoint,headers=self.headers,json=data)
+            log.debug('Response from server: {}'.format(response.text))
             response.raise_for_status()
-        except (ConnectionErrror, HTTPError, Timeout, TooManyRedirects) as err:
-            if err.response and int(err.response) < 500:
-                log.error('{}.authorise(): HTTP Error {}'.format(__name__,err.response))
+        except (ConnectionError, HTTPError, Timeout, TooManyRedirects) as e:
+            if hasattr(e,'response') and e.response.status_code < 500:
+                log.warning('Glowmarkt.authorise(): error {} - {}'.format(e.response.status_code,e.response.text))
                 raise
             else:
-                log.error('{}.authorise(): server error - aborting!'.format(__name__,err.response))
-                sys.exit()
+                log.error('Glowmarkt.authorise(): server error {} - aborting!'.format(e.response.text))
+                raise
         
-        self.token = json.load(response.json())
-        self.headers.token = self.token.token
+        if not reauth:
+            self.authObj = response.json()
+            self.headers['token'] = self.authObj['token']
+
+        log.debug('Glowmarkt.headers = {}'.format(self.headers))
         return True
     
     def __init__(self, u=None, p=None, t=None):
         self.authorise(u,p,t)
-        return self
 
 if __name__ == "__main__":
-    import sys,argparse,pickle
+    import sys, argparse, pickle
     from pathlib import Path
     from xdg import xdg_config_home
     from getpass import getpass
@@ -79,31 +85,31 @@ if __name__ == "__main__":
         log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
         log.info("Verbose output.")
     else:
-        log.basicConfig(format="%(levelname)s: %(message)s")
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.WARNING)
     
-    log.info('Called with arguments: ' + dir(args))
+    log.debug('Called with arguments: {}'.format(vars(args)))
     
     if args.config == None: config = None
     else:
         # Check and load the congifuration file
         config = Path(args.config)
-        log.info('Configuration file is {}'.format(config))
         try:
             if config.is_file():
                 log.info('Found {}'.format(config))
                 if config.stat().st_size == 0:
-                    log.info('Configuration file empty; ignoring')
+                    log.info('Configuration file {} is empty; ignoring'.format(config))
                 else:
                     with config.open('rb') as f:
                         gm = pickle.load(f)
                     if gm:
-                        log.info('Object loaded from configuration file')
+                        log.debug('gm = {}'.format(vars(gm)))
+                        gm.authorise()
+                        log.info('Configuration loaded from file {}'.format(config))
             else:
                 config.touch()
-                log.info('Created new configuration file')
-        except Exception as err:
-            log.error('Configuration file ({}) error: {}'.format(config, err))
-            sys.exit(1)
+                log.info('Created new configuration file {}'.format(config))
+        except Exception as e:
+            log.warning('Configuration file ({}) error: {}'.format(config, e))
     
     # Instantiate or update the gm object from user provided details
     if args.token:
@@ -115,7 +121,7 @@ if __name__ == "__main__":
         log.info('Token validated! Ready to query API')
     if args.user:
         log.info('Username {} provided: getting password'.format(args.user))
-        if gm.token:
+        if gm and gm.token:
             gm.token = None
             log.info('Stored token cleared; reauthorising with username and password')
         for i in range(3):
@@ -124,14 +130,18 @@ if __name__ == "__main__":
                 if gm:
                     gm.authorise(u=args.user,p=pwd)
                 else:
-                    gm = Glowmarket(u=args.user,p=pwd)
-            except HTTPError:
-                log.error('Password not accepted for user {}.'.format(args.user))
-                print('Username or password not accepted. Try again?')
+                    gm = Glowmarkt(u=args.user,p=pwd)
+            except AssertionError as e:
+                log.error(str(e))
                 continue
+            except HTTPError as e:
+                log.error('Password not accepted.')
+                continue
+            #except Exception as e:
+            #    log.error('Some other error occurred: {}'.format(e))
             else:
                 log.info('User {} authorised!'.format(args.user))
-            
+                break
 
     # Save configuration and clean up
     if config:
@@ -139,11 +149,11 @@ if __name__ == "__main__":
             try:
                 with config.open('wb') as f:
                     pickle.dump(gm,f,pickle.HIGHEST_PROTOCOL)
-            except Exception as err:
-                print('Couldn\'t save configuration to file ' + str(config) + ': ' + str(err))
+            except Exception as e:
+                log.warning('Couldn\'t save configuration to file {}: {}'.format(config, e))
         if config.is_file() and config.stat().st_size == 0:
-            print('Removing empty configuration file ' + str(config))
+            log.info('Removing empty configuration file {}'.format(config))
             try:
                 config.unlink()
-            except Exception as err:
-                print('Error deleting empty configuration file ' + str(config) + ': ' + str(err))
+            except Exception as e:
+                log.warning('Error deleting empty configuration file {}: {}'.format(config, e))
